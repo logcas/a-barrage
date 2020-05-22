00bc542cb64c6699ec52510c303978ea442ab973
everlastlucas <everlastlucas@outlook.com>
2020-05-22 14:54:49 +0800

    feat: add helper functions

---
+++ src/helper/index.ts
@@ -0,0 +1,21 @@
+export function isEmptyArray<T>(array: T[]): boolean {
+  return array.length === 0
+}
+
+export function getArrayRight<T>(array: T[]): T {
+  return array[array.length - 1]
+}
+
+export const TIME_PER_FRAME = 16.6
+
+export function getEl(el: HTMLElement | string): HTMLElement | null {
+  if (el instanceof HTMLElement) {
+    return el
+  }
+  return document.querySelector(el)
+}
+
+export const requestAnimationFrame =
+  window.requestAnimationFrame || window.webkitRequestAnimationFrame
+
+export const cancelAnimationFrame = window.cancelAnimationFrame || window.webkitCancelAnimationFrame
