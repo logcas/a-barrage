78d809eb6e52e50d39b87d381e067814a391ba8f
everlastlucas <everlastlucas@outlook.com>
2020-05-22 15:45:04 +0800

    chore: Add development environment

---
+++ example/src/index.js
@@ -0,0 +1,72 @@
+import Barrage from '../../dist/barrage.umd';
+import faker from 'faker/locale/zh_CN';
+
+function mockBarrage() {
+  return {
+    text: faker.lorem.words(),
+    color: Math.random() > 0.5 ? '#fff' : faker.internet.color().toUpperCase()
+  };
+}
+
+const $ = (selector) => document.querySelector(selector);
+
+const barrage = new Barrage('#container');
+const player = $('#my-player');
+let timer= null;
+player.onpause = () => {
+  console.log('pause');
+  clearTimeout(timer);
+  barrage.stop();
+};
+player.onplay = () => {
+  console.log('start');
+  barrage.start();
+  timer = setTimeout(function insertBarrage() {
+    let sum = 1 + Math.floor(20 * Math.random());
+    while(sum--) {
+      barrage.add(mockBarrage());
+    }
+    timer = setTimeout(insertBarrage, 2000 + Math.floor(Math.random() * 10000));
+  }, 2000);
+};
+player.onresize = () => {
+  console.log('onresize');
+  const { width } = player.getBoundingClientRect();
+  barrage.resize(width);
+};
+player.onseeked = () => {
+  barrage.clear();
+};
+
+new Vue({
+  el: '#dashboard',
+  data() {
+    return {
+      barrageText: '哔哩哔哩干杯',
+      opacity: 1
+    }
+  },
+  methods: {
+    sendBarrage() {
+      if (this.barrageText.trim() === '') {
+        this.$notify.error({
+          title: '失败',
+          message: '弹幕内容不能为空'
+        });
+        return;
+      }
+      barrage.add({
+        text: this.barrageText
+      });
+      this.barrageText = '';
+      this.$notify({
+        title: '成功',
+        message: '弹幕发送成功',
+        type: 'success'
+      });
+    },
+    opacityChange(val) {
+      barrage.setOpacity(val);
+    }
+  }
+});
---
+++ example/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    port: 9527,
+    host: 'localhost',
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?ts$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html')
+    })
+  ]
+}
