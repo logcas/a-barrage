04b9ba59a724de27a48f918abebffd1bdde725a6
everlastlucas <everlastlucas@outlook.com>
2020-05-29 20:19:04 +0800

    build: update lib name

--- rollup.config.ts
+++ rollup.config.ts
@@ -12,7 +12,7 @@ const libraryName = 'a-barrage'
 export default {
   input: `src/${libraryName}.ts`,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
+    { file: pkg.main, name: 'ABarrage', format: 'umd', sourcemap: true },
     { file: pkg.module, format: 'es', sourcemap: true },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
