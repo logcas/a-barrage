e2e4d1515c4777913561a654334ca4bf5e91be3b
everlastlucas <everlastlucas@outlook.com>
2020-06-03 21:32:31 +0800

    refactor(canvas-engine): export commanders of canvas

---
+++ src/commander/canvas/index.ts
@@ -0,0 +1,9 @@
+import FixedTopCommander from './fixed-top'
+import FixedBottomCommander from './fixed-bottom'
+import RollingCommander from './rolling'
+
+export default {
+  FixedBottomCommander,
+  FixedTopCommander,
+  RollingCommander
+}
